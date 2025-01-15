9b2f07c95fe2df83d4f5ecc0585cbf93bf033f3e
lucas.ying@menloschool.org <lucas.ying@menloschool.org>
2025-01-15 13:46:33 -0800

    update

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,9 @@
 Name: YOUR NAME HERE
 
-| Date    |  Time  |                                     Update |
-|:--------|:------:|-------------------------------------------:|
-| April 5 | 9-10pm | I tried to write abstract, efficient code. |
-|         |        |                                            |
+| Date       |     Time     |                                                                                          Update |
+|:-----------|:------------:|------------------------------------------------------------------------------------------------:|
+| January 15 | 12:35-1:45pm |  I thought about the how to solve this using an algorithm that tracks each count for every day. |
+|            |              |                                                                                                 |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/WeatherPatterns.java
+++ src/WeatherPatterns.java
@@ -3,7 +3,7 @@
  * each day’s temperature is higher than on the previous day in that sequence.
  *
  * @author Zach Blick
- * @author YOUR NAME HERE
+ * @author Lucas Ying
  */
 
 public class WeatherPatterns {
@@ -15,8 +15,29 @@ public class WeatherPatterns {
      * @return the longest run of days with increasing temperatures
      */
     public static int longestWarmingTrend(int[] temperatures) {
-        // TODO: Write your code here!
 
-        return 0;
+        int counter[] = new int[temperatures.length];
+        counter[0] = 1;
+        int longestCount;
+        if (temperatures.length == 1){
+            return 1;
+        }
+        // Count and keep each day with their corresponding count pointing to its day
+        int j = 1;
+        for (int i = 1; i < temperatures.length; i++) {
+            int current = temperatures[i];
+            if (current > temperatures[j-1]) {
+                counter[i] = counter[i-1] + 1;
+                // 73 75 74 76 77 79 78
+                // 01 02 02 03 04 05 05
+                j++;
+            }
+            else if (current < temperatures[i-1]) {
+                counter[i] = counter[i-1];
+
+            }
+        }
+        longestCount = temperatures.length - 1;
+        return counter[longestCount];
     }
 }
